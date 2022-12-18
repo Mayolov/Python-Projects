0dffb09b281fac7d28ec1eaa224998e0f1ed19f0
Mayolov <danyvalencia25@gmail.com>
2022-12-18 13:46:41 -0800

    Added some documentation

--- spotifyMacro.py
+++ spotifyMacro.py
@@ -1,6 +1,10 @@
 import webbrowser, os, psutil, pyautogui, time
 from pynput import keyboard
 
+""" check_power_state(): if the computer is plugged in continue to 
+    operate and check. Else check the battery and if its less than 20% 
+    shutdown the program to save battery.
+"""
 def check_power_state():
     if psutil.sensors_battery().power_plugged:
         # The computer is running on AC power
@@ -11,7 +15,12 @@ def check_power_state():
             
             # The battery is low, shut down the program
              os.system('shutdown /s')
-             
+
+    """ When the hotkey is press on_activate() is called this prints 
+        that its been pressed, opens the spotify webplayer, moves to the
+        center of the screen and down to the spot of the play button, waits,
+        and then clicks the button 
+    """
 def on_activate():
     # Sensed the hotkey pressed
     print('Global hotkey activated!')
@@ -24,6 +33,9 @@ def on_activate():
     # Clicks the pause button
     pyautogui.click()
 
+""" Allows for 'H' and 'h' or the shifts and controls on either side to be 
+    pressed
+"""
 def for_canonical(f):
     return lambda k: f(l.canonical(k))
 
@@ -36,6 +48,7 @@ with keyboard.Listener(on_press=for_canonical(hotkey.press),
     on_release=for_canonical(hotkey.release)) as l: 
     l.join()
     
+    # this loops needs help to be started, the top function is blocking.
     while True:
         
         if(count == 600):
